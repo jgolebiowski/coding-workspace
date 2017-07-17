58326869061539492cf8b4219c49b40aa76564cc
Jacek Golebiowski <jacek.golebiowski14@imperial.ac.uk>
2017-07-17 08:42:08 +0100

    deleted old examples (were not woking anyways)

--- deepLearning-google/simpleNeuralNetwork/mlModule/adaptiveSGD.py
+++ deepLearning-google/simpleNeuralNetwork/mlModule/adaptiveSGD.py
@@ -95,7 +95,7 @@ class adaptiveSGD(object):
                     self.costLists.append(iterCost)
                     print("Mibatch: %d out of %d from epoch: %d out of %d, iterCost is: %f" %
                           (indexMB, self.nMiniBatches, indexE, self.epochs, iterCost))
-                    print "Median of the learning rate is", np.median(self.learningRate)
+                    # print "Median of the learning rate is", np.median(self.learningRate)
                     iterCost = 0
 
         return self.params
