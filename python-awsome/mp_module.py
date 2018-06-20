f58915a6849ef7419f83f3f41d711611dd695ebc
Jacek Golebiowski <golejace@amazon.com>
2018-06-20 09:29:16 +0100

    Updated mp module

--- python-awsome/mp_module.py
+++ python-awsome/mp_module.py
@@ -54,6 +54,7 @@ def paralll_worker(rank, size,
         Quete to store the results in
     """
     for input in batch:
+        print("This is process {} out of {} operating on {}".format(rank, size, input))
         result = target_function(*input, *fixed_args)
         output_queue.put((input, result))
 
@@ -77,7 +78,7 @@ def parallel_control(target_function, list2process, fixed_args=None, num_threads
         (inuput, output)
     """
     if num_threads is None:
-        num_threads = mp.cpu_count()
+        num_threads = min(mp.cpu_count(), len(list2process))
     if fixed_args is None:
         fixed_args = ()
 
